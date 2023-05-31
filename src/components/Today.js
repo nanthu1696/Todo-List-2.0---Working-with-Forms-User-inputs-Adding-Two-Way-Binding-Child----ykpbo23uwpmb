ea831b1aa0cb4e86af8eeec222e6f947aa1a2865
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-06-01 00:17:54 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import ListRender from "./ListRender";
 
 const Today = (props) => {
+  let {list} = props;
   const date = new Date();
-  let arr = [];
+  let [arr , setArr] = useState([]);
+  useEffect(() =>{
+    let newarr = [];
   for (let i = 0; i < props.list.length; i++) {
     if (props.list[i].date.getFullYear() === date.getFullYear()) {
       if (props.list[i].date.getMonth() === date.getMonth()) {
         if (props.list[i].date.getDate() === date.getDate()) {
-          arr.push(props.list[i]);
+          newarr.push(props.list[i]);
         }
       }
     }
   }
+    setArr(newarr);
+  }, [list]);
   
   return (
     <div id="today-list">
