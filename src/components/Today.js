7dd3e820b1f4abadc0be18040038e26a10d9f304
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-06-01 00:08:17 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,6 +3,16 @@ import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
+  let arr = [];
+  for (let i = 0; i < props.list.length; i++) {
+    if (props.list[i].date.getFullYear() === date.getFullYear()) {
+      if (props.list[i].date.getMonth() === date.getMonth()) {
+        if (props.list[i].date.getDate() === date.getDate()) {
+          arr.push(props.list[i]);
+        }
+      }
+    }
+  }
   
   return (
     <div id="today-list">
